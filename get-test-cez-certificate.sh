a763319b1ec863eaa79649caa34a32571d78fee0
Krzysztof Tomasz Zembrowski <zembrowski@users.noreply.github.com>
2024-06-10 15:44:17 +0200

    fix: remove filename duplication

--- get-test-cez-certificate.sh
+++ get-test-cez-certificate.sh
@@ -3,6 +3,6 @@
 TARGET_DIRECTORY="CEZ/"
 CERTIFICATE_ARCHIVE_FILENAME="Test_Zertifikate.zip"
 
-curl -sS -o "$TARGET_DIRECTORY"Test_Zertifikate.zip https://download.elster.de/download/schnittstellen/"$CERTIFICATE_ARCHIVE_FILENAME" && \
+curl -sS -o "$TARGET_DIRECTORY$CERTIFICATE_ARCHIVE_FILENAME" https://download.elster.de/download/schnittstellen/"$CERTIFICATE_ARCHIVE_FILENAME" && \
 unzip -j "$TARGET_DIRECTORY$CERTIFICATE_ARCHIVE_FILENAME" -d "$TARGET_DIRECTORY" eric-zertifikate-bescheidabholung/PSE/eric_private.p12 eric-zertifikate-bescheidabholung/PSE/eric_public.cer && \
 rm "$TARGET_DIRECTORY$CERTIFICATE_ARCHIVE_FILENAME"
